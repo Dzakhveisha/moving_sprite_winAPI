a338b3a419b841903ddedb2ae523cd51baf17d1d
Dzakhveisha <Dzakhveisha@gmail.com>
2021-09-07 18:21:57 +0300

    Добавьте файлы проекта.

---
+++ moving_sprite/desktop.cpp
@@ -0,0 +1,70 @@
+#include <windows.h>
+#include <string.h>
+#include <tchar.h>
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("DesktopApp");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Windows Desktop Guided Tour Application");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+    WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR     lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    WNDCLASSEX wcex; HWND hWnd; MSG msg;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_DBLCLKS;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = wcex.hIcon;
+
+    RegisterClassEx(&wcex);
+
+    hWnd = CreateWindow(szWindowClass, szTitle,
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0,
+        CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+ }                
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+    WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_LBUTTONDBLCLK:
+        MessageBox(hWnd, szTitle, szTitle, MB_OK);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+
